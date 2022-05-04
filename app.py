a5efedd2ef166270eb6720ca9e4a46ebc5fe4710
Roshu2 <kyumin1020@gmail.com>
2022-05-05 00:05:02 +0900

    1. 댓글DB저장 기능 2. 댓글 포스트 박스 표시기능 추가 3. 게시글 추가기능

--- app.py
+++ app.py
@@ -7,19 +7,53 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
+
+
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
+# 홈페이지 불러오기
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+# 개인페이지 불러오기
 @app.route('/user')
 def user():
     return render_template('user.html')
+
+# 새로운 게시글 작성
+@app.route("/main", methods=["POST"])
+def new_post():
+    posting_picture_receive = request.form['posting_picture_give']
+    posting_comment_receive = request.form['posting_comment_give']
+
+    doc = {
+        'posting_picture': posting_picture_receive,
+        'posting_comment': posting_comment_receive
+    }
+
+    db.posts.insert_one(doc)
+    return jsonify({'msg':'작성완료!'})
+
+# 게시글에 댓글 작성
+@app.route("/main/post", methods=["POST"])
+def new_comment():
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'post_comment': comment_receive
+    }
+
+    db.posts.insert_one(doc)
+    return jsonify({'msg':'댓글작성완료!'})
+
+
+@app.route("/main/post", methods=["GET"])
+def post_comment():
+    comment_list = list(db.posts.find({}, {'_id': False}))
+    return jsonify({'comments':comment_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/scripts/mainscripts.js
+++ static/scripts/mainscripts.js
@@ -23,13 +23,65 @@ modal.addEventListener("click", e => {
 });
 
 
-function add_comment() {
-    let comment = $('#exampleFormControlInput1').val()
-    let temp_html = `<p style="font-weight: lighter"><span style="font-weight: bold">Kang_Jun</span>${comment}</p>`
-    $('#comment-q1').append(temp_html)
-}
+// // function add_comment() {
+// //     let comment = $('#exampleFormControlInput1').val()
+// //     let temp_html = `<p style="font-weight: lighter"><span style="font-weight: bold">Kang_Jun</span>${comment}</p>`
+// //     $('#comment-q1').append(temp_html)
+// }
 
 function q2() {
     let post = $('#feed_box')
     $('#feed').append(post)
+}
+
+$(document).ready(function () {
+    show_comment()
+});
+
+
+function save_post() {
+    let posting_picture = $('#posting_picture').val()
+    let posting_comment = $('#posting_comment').val()
+
+    $.ajax({
+        type: 'POST',
+        url: '/main',
+        data: {posting_picture_give: posting_picture, posting_comment_give: posting_comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+function add_comment() {
+    let comment = $('#exampleFormControlInput1').val()
+
+    $.ajax({
+        type: 'POST',
+        url: '/main/post',
+        data: {comment_give: comment},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+function show_comment() {
+    $.ajax({
+        type: "GET",
+        url: "/main/post",
+        data: {},
+        success: function (response) {
+            let rows = response['comments']
+            for (let i = 0; i < rows.length; i++) {
+                let comment = rows[i]['post_comment']
+
+                let temp_html = `<p style="font-weight: lighter"><span style="font-weight: bold">Kang_Jun</span> ${comment}</p>`
+
+                $('#comment-q1').append(temp_html)
+            }
+        }
+    });
 }
