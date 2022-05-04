72a467a765e2dfeba2ec4bcd6ad4dfd8c9221472
Lee-DH <102156247+DHL68@users.noreply.github.com>
2022-05-04 15:33:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.bmyui.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('')
 db = client.dbsparta
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
