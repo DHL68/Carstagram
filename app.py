cf069b78d5b934e3ebc13ea91ea2a3c601f98e6c
DHL68 <ldh1255@naver.com>
2022-05-05 23:22:10 +0900

    feat : 동기화 완료 DH

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-=======
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 from datetime import datetime
@@ -8,59 +7,25 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
->>>>>>> personal_branch_DH
 from pymongo import MongoClient
-<<<<<<< HEAD
-=======
-client = MongoClient('localhost', 27017)
-<<<<<<< HEAD
-db = client.carstagram
->>>>>>> personal_branch_DH
 
 client = MongoClient('localhost', 27017)
-db = client.Carstagram
+db = client.carstagram
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
-# 저장 - 예시
-doc = {'name':'bobby','age':21}
-db.carstagram.insert_one(doc)
-
-from flask import Flask, render_template, jsonify, request
-
-app = Flask(__name__)
-
-<<<<<<< HEAD
 # 홈페이지 불러오기
 @app.route('/')
 def main():
     return render_template('index.html')
-=======
-@app.route('/')
-def home():
-    return render_template('main.html')
->>>>>>> personal_branch_DH
+
 
 # 개인페이지 불러오기
 @app.route('/user')
 def user():
     return render_template('user.html')
-
-# 새로운 게시글 작성
-@app.route("/main", methods=["POST"])
-def new_post():
-    posting_picture_receive = request.form['posting_picture_give']
-    posting_comment_receive = request.form['posting_comment_give']
-
-    doc = {
-        'posting_picture': posting_picture_receive,
-        'posting_comment': posting_comment_receive
-    }
-
-    db.posts.insert_one(doc)
-    return jsonify({'msg':'작성완료!'})
 
 
 @app.route("/comment", methods=["POST"])
@@ -68,89 +33,19 @@ def new_comment():
     comment_receive = request.form['comment_give']
 
     doc = {
-        'comment': comment_receive
+        'comments': comment_receive
     }
-
     db.comments.insert_one(doc)
-    return jsonify({'msg':'댓글작성완료!'})
+
+    return jsonify({'msg': '댓글작성완료!'})
 
 
 @app.route("/comment", methods=["GET"])
 def comment():
     comment_list = list(db.comments.find({}, {'_id': False}))
-    return jsonify({'comments':comment_list})
 
+    return jsonify({'comments': comment_list})
 
-
-
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-=======
-
-# 개인 글쓰기(POST) API
-# 해당 페이지의 경로 지정, 메서드 구분 지정
-@app.route('/personal', methods=['GET', 'POST'])
-# write 함수 지정
-def write():
-    # 조건문 지정 post 메서드 일 때
-    if request.method == "POST":
-        cur_time = time.strftime("%y%m%d_%H%M%S")
-        title = request.form.get('title')
-        contents = request.form.get('contents')
-        # year = request.form.get('year')
-        # month = request.form.get('month')
-        # day = request.form.get('day')
-        # date = year + "년 " + month + "월 " + day +"일"
-        if 'email' in session:
-            id = session['email']
-
-        # DB 에 저장 (값의 테이블 지정)
-        db = {
-            'email': id,
-            'title': title,
-            'contents': contents,
-            'pubdate': cur_time,
-            'date': date
-        }
-        # 저장
-        aaa.insert_one(db)
-
-        redirect(url_for('bulletin_rd'))
-        return jsonify({'ans': 'success', 'msg': "작성 완료"})
-    else:
-        return render_template('personal_main.html')
-
-
-
-# 개인 글보기(GET) API
-@app.route("/bulletin_rd", methods=['GET'])
-def bulletin_rd():
-    diary_data = list(aaa.find({'email':session['email']}, {'_id': False}).sort('date', 1))
-    print(diary_data)
-    return jsonify({'all_data': diary_data})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-db = client.dbparc
-
-## HTML을 주는 부분
-@app.route('/')
-def home():
-   return render_template('main.html')
->>>>>>> personal_branch_DH
 
 #
 # post 리스팅 메서드
@@ -185,7 +80,6 @@ def post_posting():
     save_to = f'static/{filename}.{extension}'
     post_pic.save(save_to)
 
-
     doc = {
         'post_pictures': picture_receive,
         'post_comments': comment_receive,
@@ -193,13 +87,8 @@ def post_posting():
     }
     db.post.insert_one(doc)
 
-    return jsonify({'msg':'업로드 완료!'})
+    return jsonify({'msg': '업로드 완료!'})
+
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run('0.0.0.0', port=5500, debug=True)
-
->>>>>>> personal_branch_DH
-=======
-   app.run('0.0.0.0',port=5000,debug=True)
->>>>>>> personal_branch_DH
--- static/scripts/post.js
+++ static/scripts/post.js
@@ -20,82 +20,88 @@ function post_listing() {
                 let post_pic = posts[i]['post_pic']
 
                 let temp_html =`
-                        <div class="feed-box">
-                        <!--                상단의 이미지와 이름 그리고 ''' 아이콘-------------------------------------------------->
-                        <div style="display: flex; align-items: center; justify-content: space-between; margin-top: 2%; margin-left: 1%; margin-bottom: 2%;">
-                            <div style="display: flex; align-items: center">
-                                <div class="box-profile">
-                                    <a href=""><img class="profile"
-                                                    src="https://upload.wikimedia.org/wikipedia/commons/d/d4/170821_%EC%98%A4%EC%97%B0%EC%84%9C_09.jpg"></a>
+                            <div class="feed-box" id="feed_box">
+                                <!--                상단의 이미지와 이름 그리고 ''' 아이콘-------------------------------------------------->
+                                <div style="display: flex; align-items: center; justify-content: space-between; margin-top: 2%; margin-left: 1%; margin-bottom: 2%;">
+                                    <div style="display: flex; align-items: center">
+                                        <div class="box-profile">
+                                            <a href=""><img class="profile"
+                                                            src="http://kaihuastudio.com/common/img/default_profile.png"></a>
+                                        </div>
+                                        <div style="margin-left: 10px">
+                                            IlikeCar
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button id="open-modal" class="btn-open-popup" style="border: none; background-color: white;">
+                                            <span class="material-icons-outlined" style="margin-right: 15px;">more_horiz</span>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div style="margin-left: 10px">
-                                    OhYeonSeo
+                                <!--                메인 피드 슬라이드 사진--------------------------------------------------------------->
+                                <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false"
+                                     data-bs-interval="false" style="height: 600px;">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="../static/${post_pic}"
+                                                 class="d-block w-100 feed-picture" alt="...">
+                                        </div>
+                                    </div>
+                                    <button class="carousel-control-prev" type="button"
+                                            data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button"
+                                            data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
                                 </div>
-                            </div>
-                            <div>
-                                <button id="open-modal2" class="btn-open-popup" style="border: none; background-color: white;">
-                                    <span class="material-icons-outlined" style="margin-right: 15px;">more_horiz</span>
-                                </button>
-                            </div>
-                        </div>
-                        <!--                메인 피드 슬라이드 사진--------------------------------------------------------------->
-                        <div id="carouselExampleControls" class="carousel slide" data-bs-interval="false">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="../static/${post_pic}"
-                                         class="d-block w-100" alt="...">
+                                <!--                사진 아래 아이콘--------------------------------------------------------------------->
+                                <div style="margin-top: 2%; display: flex; flex-direction: row; justify-content: space-between;">
+                                    <div style="margin-left: 10px;">
+                                        <button class="like-btn" onclick="like()"><span class="material-icons-outlined">favorite_border</span></button>
+                                        <span class="material-icons-outlined">mode_comment</span>
+                                        <span class="material-icons-outlined">send</span>
+                                    </div>
+                                    <div style="margin-right: 10px;">
+                                        <span class="material-icons-outlined">bookmark_border</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                    data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                    data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                        <!--                사진 아래 아이콘--------------------------------------------------------------------->
-                        <div style="margin-top: 2%; display: flex; flex-direction: row; justify-content: space-between;">
-                            <div style="margin-left: 10px;">
-                                <span class="material-icons-outlined">favorite_border</span>
-                                <span class="material-icons-outlined">mode_comment</span>
-                                <span class="material-icons-outlined">send</span>
-                            </div>
-                            <div style="margin-right: 10px;">
-                                <span class="material-icons-outlined">bookmark_border</span>
-                            </div>
-                        </div>
-                        <!--            아이콘 아래 텍스트 부분-------------------------------------------------------------------->
-                        <div style="margin-left: 10px;">
-                            <!--                좋아요-->
-                            <div>좋아요 72,432개</div>
-                            <!--                    게시자 글-->
-                            <div>BoYoung <span style="font-weight: lighter; color: dodgerblue;">${post_picture}</span><b
-                        style="font-weight: lighter">${post_comment}</b></div>
-                            <!--                댓글모두보기-->
-                            <div style="font-weight: lighter; color: grey">댓글 5,621개 모두보기</div>
-                            <!--                댓글-->
-                            <div>K_sometimes1 <span style="font-weight: lighter">오늘도 미모 열일 하시네여 !</span></div>
-                            <!--                몇일,시간,분전-->
-                            <div style="font-weight: lighter; font-size: 10px;">2일전</div>
-                            <!--                댓글달기-->
-                            <div style="display:flex; flex-direction: row; justify-content: center; margin-left: -10px; margin-top: 10px; border-top: solid 1px #dbdbdb;">
-                                <span style="margin-left: 8px; margin-top: 7px;" class="material-symbols-outlined">mood</span>
-                                <input type="email" class="form-control"
-                                       style="box-shadow: none; border: none; border-radius: 0px;" id="exampleFormControlInput1"
-                                       placeholder="댓글 달기 ...">
-                                <a href="#"
-                                   style="width: 50px; margin-right: 8px; margin-top: 7px; text-decoration: none; color: cornflowerblue; font-weight: bold">
-                                    게시
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    `
+                                <div style="margin-left: 10px;">
+                                    <!--                좋아요-->
+                                    <div>좋아요 1개</div>
+                                    <!--                    게시자 글-->
+                                    <div>
+                                        <div><a href='#' class="name">IlikeCar</a>
+                                        <span style="font-weight: lighter; color: dodgerblue;">
+                                        ${post_picture}
+                                        </span>
+                                        <span style="font-weight: lighter">${post_comment}</span>
+                                        <span style="color: #c7c7c7">더보기</span>
+                                        </div>
+                                    </div>
+                                    <!--                댓글모두보기-->
+                                    <!--<div style="font-weight: lighter; color: grey">댓글 3,243개 모두보기</div>-->
+                                    <!--                댓글-->
+                                    <div id="comment-q1" class="comment-box">
+                    
+                                    </div>
+                                    <!--                몇일,시간,분전-->
+                                    <div style="font-weight: lighter; font-size: 10px;">3시간전</div>
+                                    <!--                댓글달기-->
+                                    <div style="display:flex; flex-direction: row; justify-content: center; margin-left: -10px; margin-top: 10px; border-top: solid 1px #dbdbdb;">
+                                        <span style="margin-left: 8px; margin-top: 7px;" class="material-symbols-outlined">mood</span>
+                                        <input type="text" class="form-control"
+                                               style="box-shadow: none; border: none; border-radius: 0px;" id="exampleFormControlInput1"
+                                               placeholder="댓글 달기 ..."/>
+                                        <button onclick="add_comment()"
+                                                style="background-color: white; border: none; width: 50px; margin-right: 8px; text-decoration: none; color: cornflowerblue; font-weight: bold">
+                                            게시
+                                        </button>
+                                    </div>
+                                </div>`
                 $('#post-feed-box').append(temp_html)
             }
         }
