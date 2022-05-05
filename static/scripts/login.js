bbe7ea7a309ae54b2a2d337149652260e7aeccd5
creamone <gsukim777@gmail.com>
2022-05-05 23:17:17 +0900

    로그인페이지 + 회원가입페이지 db에 정보 쌓기 성공

---
+++ static/scripts/login.js
@@ -0,0 +1,67 @@
+// 로그인
+function login() {
+    $.ajax({
+        type: "POST",
+        url: "/api/login",
+        data: {id_give: $('#userid').val(), pw_give: $('#userpw').val()},
+        success: function (response) {
+            if (response['result'] == 'success') {
+                // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                $.cookie('mytoken', response['token']);
+
+                alert('로그인 완료!')
+                window.location.href = '/'
+            } else {
+                // 로그인이 안되면 에러메시지를 띄웁니다.
+                alert(response['msg'])
+            }
+        }
+    })
+}
+
+
+// 로그인 클라이언트
+function sign_in() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+
+    if (username == "") {
+        $("#help-id-login").text("아이디를 입력해주세요.")
+        $("#input-username").focus()
+        return;
+    } else {
+        $("#help-id-login").text("")
+    }
+
+    if (password == "") {
+        $("#help-password-login").text("비밀번호를 입력해주세요.")
+        $("#input-password").focus()
+        return;
+    } else {
+        $("#help-password-login").text("")
+    }
+    $.ajax({
+        type: "POST",
+        url: "/sign_in",
+        data: {
+            username_give: username,
+            password_give: password
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                $.cookie('mytoken', response['token'], {path: '/'});
+                window.location.replace("/")
+            } else {
+                alert(response['msg'])
+            }
+        }
+    });
+}
+
+// 로그아웃은 내가 가지고 있는 토큰만 쿠키에서 없애면 됩니다.
+function logout() {
+    $.removeCookie('mytoken');
+    alert('로그아웃!')
+    window.location.href = '/login'
+}
