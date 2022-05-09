e39b89a5b998a60cdb2c7243385dd0aee91686ec
Roshu2 <kyumin1020@gmail.com>
2022-05-09 20:21:49 +0900

    1. js 빈칸수정 불필요 파일 제거

--- static/scripts/sign_up.js
+++ static/scripts/sign_up.js
@@ -70,7 +70,6 @@ function check_dup() {
                 $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")
             }
             $("#help-id").removeClass("is-loading")
-
         }
     });
 }
