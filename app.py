af667e5ca21a6e009b29cf9aee1ace73bcee71f5
DHL68 <ldh1255@naver.com>
2022-05-10 22:29:12 +0900

    4차 수정

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ def home():
     try:
         # 암호화되어있는 token의 값을 우리가 사용할 수 있도록 디코딩(암호화 풀기)해줍니다!
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"email": payload['email']})
+        email = db.users.find_one({"email": payload['email']})
 
         # print(user_info)
 
-        return render_template('main.html', email=user_info["email"])
+        return render_template('main.html', email=email["email"])
 
     # 만약 해당 token의 로그인 시간이 만료되었다면, 아래와 같은 코드를 실행합니다.
     except jwt.ExpiredSignatureError:
@@ -67,7 +67,7 @@ def user_page(user_email):
     try:
 
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status_email = (user_email == payload["email"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+        status_email = (user_email == payload['email'])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
 
         print(user_email)
         print(payload)
