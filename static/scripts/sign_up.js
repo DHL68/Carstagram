dab1dfee257e35d18d0c2906fa01dba712f07f1b
Roshu2 <kyumin1020@gmail.com>
2022-05-11 09:57:55 +0900

    1. 불필요 파일 제거 2. js파일 css 파일명 _ 통일

--- static/scripts/sign_up.js
+++ static/scripts/sign_up.js
@@ -20,6 +20,11 @@ function sign_up() {
     } else if (userpw == "") {
         alert("비밀번호를 입력해주세요.")
         return;
+    } else if (useremail.includes('@') == true) {
+
+    } else {
+        alert('이메일 형식으로 입력해주세요!')
+        return;
     }
 
     $.ajax({
